2b1ac38f8efa2ab756d6384ada7df965a4f18e3f
UENISHI Kota <kuenishi@gmail.com>
2016-07-12 01:52:59 +0900

    Force signing commit

--- publish.sh
+++ publish.sh
@@ -9,7 +9,7 @@ git add *.html
 git add _static/*
 git add _sources/*
 git add _images/*
-git commit -m "autocommit by publish.sh"
+git commit -S -m "autocommit by publish.sh"
 git push origin master
 
 
