4d8601cd9ff69eaadc694244fc749e6460d8e8f3
UENISHI Kota <kuenishi@gmail.com>
2018-03-21 22:41:29 +0900

    Update copyright years'

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'kuenishi (7)'
-copyright = u'2012, UENISHI Kota'
+copyright = u'2012-2018, UENISHI Kota'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
