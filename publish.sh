130681aa0ab44ff69b331f29d8787afaf426099f
UENISHI Kota <kuenishi@gmail.com>
2019-01-04 22:29:58 +0900

    Update publish script

--- publish.sh
+++ publish.sh
@@ -8,6 +8,7 @@ cd kuenishi.github.com
 git add *.html
 git add _static/*
 git add _sources/*
+git add _sources/memo/*
 git add _images/*
 git commit -S -m "autocommit by publish.sh"
 git push origin master
