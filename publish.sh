2ff31bfc246e7999363eba1cae0995f3311ae6ef
UENISHI Kota <kuenishi@gmail.com>
2024-04-12 23:55:13 +0900

    Update much

--- publish.sh
+++ publish.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-cd build
+set -eux
+
+SRC=build/html
+
 rm -rf kuenishi.github.com/_static
 rm -rf kuenishi.github.com/_sources
 rm -rf kuenishi.github.com/memo
-cp -r html/* kuenishi.github.com
+cp -r $SRC/* kuenishi.github.com
 cd kuenishi.github.com
-git add *.html
-git add memo/*.html
-git add _static/*
-git add _sources/*
-git add _sources/memo/*
-git add _images/*
+git add -A
+#.html
+#git add memo/*.html
+#git add _static/*
+#git add _sources/*
+#git add _sources/memo/*
+#git add _images/*
 git commit -S -m "autocommit by publish.sh"
-git push origin master
+#git push origin master
 
 
